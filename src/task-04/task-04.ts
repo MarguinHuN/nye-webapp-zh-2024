79e376239295c21e7496406322122346d3b8df61
MarguinHuN <marguinhun@gmail.com>
2024-03-25 00:52:37 +0100

    task-01, task-04

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,13 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let score = 0;
+  const losePoint = Points.get(Outcome.LOOSE);
+  const drawPoint = Points.get(Outcome.DRAW);
+  const winPoint = Points.get(Outcome.WIN);
+  const rockPoint = Points.get(Shape.ROCK);
+  const paperPoint = Points.get(Shape.PAPER);
+  const scissorsPoint = Points.get(Shape.SCISSORS);
+
+  return score;
 };
